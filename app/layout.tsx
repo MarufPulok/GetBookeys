2c48a80a2998768961837346712289f679720660
MarufPulok <maruf12@student.sust.edu>
2023-07-05 17:55:52 +0600

    auth ui inp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,32 @@
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import {Nunito} from "next/font/google"
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
 export const metadata = {
-  title: 'GetBookeys',
-  description: 'GetBookeys is a book rental service.',
-}
+  title: "GetBookeys",
+  description: "GetBookeys is a book rental service.",
+};
 
 const font = Nunito({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
-        {children}</body>
+        <ClientOnly>
+          <Modal isOpen/>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
