d45766cc9f1192c170b42c50391f3d6c225d7d41
MarufPulok <maruf12@student.sust.edu>
2023-07-07 17:00:56 +0600

    github and google signin

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,9 +1,19 @@
-'use client';
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
 
-export default function Avatar() {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+export default function Avatar({ src }: AvatarProps) {
   return (
-    <Image className='rounded-full' height={30} width={30} src="/images/placeholder.jpg" alt="Avatar" />
-  )
+    <Image
+      className="rounded-full"
+      height={30}
+      width={30}
+      src={src || "/images/placeholder.jpg"}
+      alt="Avatar"
+    />
+  );
 }
--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -81,13 +81,19 @@ export default function LoginModal() {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => {
+          signIn("google", {
+            callbackUrl: "http://localhost:3000/",
+          });
+        }}
       />
       <Button
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => {
+          signIn("github");
+        }}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center justify-center gap-2">
