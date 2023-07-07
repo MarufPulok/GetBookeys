00463abbfe272cae3e8d0ab08b217510c3084bc6
MarufPulok <maruf12@student.sust.edu>
2023-07-08 01:00:22 +0600

    category wip

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -15,22 +15,42 @@ export default function CategoryBox({
   label,
   selected,
 }: CategoryBoxProps) {
-    const router = useRouter();
-    const params = useSearchParams();
+  const router = useRouter();
+  const params = useSearchParams();
 
-    const handleClick = useCallback(() => {
-        let currentQuery = {};
-        
-    }, []);
-  return (
-    <div
-      className={`flex flex-col items-center justify-center gap-2 p-2 border-b-2 hover:text-neutral-500 transition cursor-pointer
-    ${
-      selected
-        ? "border-b-neutral-800 text-neutral-800"
-        : "border-transparent text-neutral-500"
+  // category search, update url
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
     }
 
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex flex-col items-center justify-center gap-2 p-2 border-b-2 hover:text-rose-500 transition cursor-pointer
+      ${selected ? "border-b-neutral-800" : "border-transparent"}
+      ${selected ? "text-rose-500" : "text-neutral-500"}
     `}
     >
       <Icon size={26} />
--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,30 +1,46 @@
 'use client';
 import React from 'react'
 import Container from '../Container';
-import {TbBeach} from 'react-icons/tb'
+import {TbBeach, TbMountain, TbPool} from 'react-icons/tb'
 import {GiWindmill} from 'react-icons/gi'
 import {MdOutlineVilla} from 'react-icons/md'
 import CategoryBox from '../CategoryBox';
+import { useSearchParams, usePathname } from 'next/navigation';
 
 export const categories = [
   {
     label: "Beach",
     icon: TbBeach,
-    description: "This property is close to the beach",
+    description: "This property is close to the beach!",
   },
   {
     label: "Windmills",
     icon: GiWindmill,
-    description: "This property has windmills",
+    description: "This property has windmills!",
   },
   {
     label: "Modern",
     icon: MdOutlineVilla,
-    description: "This property is modern",
+    description: "This property is modern!",
+  },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description: "This property is in the countryside!",
+  },
+  {
+    label: "Pools",
+    icon: TbPool,
+    description: "This property has a pool!",
   },
 ];
 
 export default function Categories() {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathName = usePathname();
+  const isMainPage = pathName === "/";
+
   return (
     <Container>
         <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
@@ -32,7 +48,7 @@ export default function Categories() {
         <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
         />
     ))}
